8d91388471365f2354ef51074e103acc3eea446f
Christos Tsoutsas <christsoytsas@gmail.com>
2024-05-03 15:02:47 +0300

    particlesV2

--- js/app.js
+++ js/app.js
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#DAF7EC",
+        "color": "#EEEEEE",
         "opacity": 0.4,
         "width": 1
       },
